4682541268317216e737cbc49fb9d6557f166c22
neizarapriansyah <apriansyahneizar@gmail.com>
2026-04-14 21:26:37 +0700

    update Communities

--- src/api/components/communities/community.routes.js
+++ src/api/components/communities/community.routes.js
@@ -3,16 +3,8 @@ const router = express.Router();
 const communityController = require('./community.controller');
 const { authenticate } = require('../../../middlewares/auth.middleware');
 
-/**
- * Community Routes
- * Base path: /api/communities (didaftarkan di src/api/routes.js)
- */
-
-// Public routes
 router.get('/', communityController.getAllCommunities);
 router.get('/:id', communityController.getCommunityById);
-
-// Protected routes (require JWT)
 router.post('/', authenticate, communityController.createCommunity);
 router.post('/:id/join', authenticate, communityController.joinCommunity);
 router.post('/:id/leave', authenticate, communityController.leaveCommunity);
