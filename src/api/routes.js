e2ada04d11fc8744709d9cf7d87f51d5e1116daa
fakhzulraflis <rfakhzul@gmail.com>
2026-04-17 18:26:20 +0700

    newest commit

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,12 +1,12 @@
-const express = require("express");
+const express = require('express');
 
-const users = require("./components/users/users-route");
-const comments = require("./components/comments/comments-route.js");
-const bookmarks = require("./components/bookmarks/bookmarks-route.js");
-const notifications = require("./components/notifications/notifications-route.js");
-const auth = require("./components/auth/auth-route");
-const tweets = require("./components/tweets/tweets-route");
-const repost = require("./components/repost/repost-route");
+const users = require('./components/users/users-route');
+const comments = require('./components/comments/comments-route');
+const bookmarks = require('./components/bookmarks/bookmarks-route');
+const notifications = require('./components/notifications/notifications-route');
+const auth = require('./components/auth/auth-route');
+const tweets = require('./components/tweets/tweets-route');
+const repost = require('./components/repost/repost-route');
 
 module.exports = () => {
   const app = express.Router();
