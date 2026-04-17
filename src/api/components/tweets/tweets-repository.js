234325e98166b68777ecbd3109dba36e17877cd6
SherlyTamaraa <sherlytamara07@gmail.com>
2026-04-17 17:47:50 +0700

    Melengkapi endpoint tweets

--- src/api/components/tweets/tweets-repository.js
+++ src/api/components/tweets/tweets-repository.js
@@ -1,4 +1,4 @@
-const { Tweets } = require("../../../models");
+const { Tweets } = require('../../../models');
 
 async function createTweet(userId, username, text) {
   return Tweets.create({
@@ -12,7 +12,25 @@ async function getTweetByTweetId(tweetId) {
   return Tweets.findOne({ tweetId });
 }
 
+// hapus tweets tertentu
+async function deleteTweetByTweetId(tweetId) {
+  return Tweets.deleteOne({ tweetId });
+}
+
+// ambil tweets terbaru
+async function getRecentTweets() {
+  return Tweets.find().sort({ createdAt: -1 }).limit(10);
+}
+
+// ambil semua tweet punya user tertentu
+async function getTweetsByUserId(userId) {
+  return Tweets.find({ userId });
+}
+
 module.exports = {
   createTweet,
   getTweetByTweetId,
+  deleteTweetByTweetId,
+  getRecentTweets,
+  getTweetsByUserId,
 };
